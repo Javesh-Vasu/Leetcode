3089ab8b446a6986ea58c80a83499284a71cfcf9
Javesh-Vasu <javeshkhungar@gmail.com>
2022-05-08 09:09:52 +0530

    Leetcode-1

---
+++ 1. Two Sum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ans(2);
+        unordered_map<int,int> complements;
+        
+        for ( int i = 0 ; i < nums.size() ; i++ )
+        {
+            complements[nums[i]] = i;
+        }
+        
+        for ( int i = 0 ; i < nums.size() ; i++ )
+        {
+            if ( complements.find(target-nums[i]) != complements.end() && complements[target-nums[i]] != i)
+            {
+                ans[0] = i;
+                ans[1] = complements[target-nums[i]];
+                break;
+            }
+        }
+        return ans;
+        
+    }
+};
